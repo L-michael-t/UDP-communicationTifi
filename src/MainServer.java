7d3f1859da8fa6f3188763e781fca7813633c08f
diegoscuolaitis-tech <diegoscuolaitis@gmail.com>
2026-02-24 14:50:19 +0100

    second

--- src/MainServer.java
+++ src/MainServer.java
@@ -1,2 +1,14 @@
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
 public class MainServer {
+    DatagramSocket ds;
+
+    {
+        try {
+            ds = new DatagramSocket(3989);
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
