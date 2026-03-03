b56f12b8380b1f9b8f5f21d90c1da52cad4e8a4a
diegoscuolaitis-tech <diegoscuolaitis@gmail.com>
2026-03-03 14:48:20 +0100

    commit

--- src/MainClient.java
+++ src/MainClient.java
@@ -1,16 +1,32 @@
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.SocketException;
+import java.net.InetAddress;
 
 public class MainClient {
-        DatagramSocket ds;
 
-        {
-            try {
-                ds = new DatagramSocket();
-            } catch (SocketException e) {
-                throw new RuntimeException(e);
-            }
-        }
+    public static void main(String[] args) throws Exception {
 
+        DatagramSocket ds = new DatagramSocket();
 
+        String messaggio = "bella a tutti ragzzi";
+        byte[] buffer = messaggio.getBytes();
+
+        InetAddress serverAddress = InetAddress.getByName("tifuxhost");
+
+        DatagramPacket packet =
+                new DatagramPacket(buffer, buffer.length, serverAddress, 3989);
+
+        ds.send(packet);
+
+        byte[] rispostaBuffer = new byte[256];
+        DatagramPacket rispostaPacket =
+                new DatagramPacket(rispostaBuffer, rispostaBuffer.length);
+
+        ds.receive(rispostaPacket);
+
+        String risposta = new String(rispostaPacket.getData(), 0, rispostaPacket.getLength());
+        System.out.println("risposta dal server: " + risposta);
+
+        ds.close();
+    }
 }
--- src/MainServer.java
+++ src/MainServer.java
@@ -1,14 +1,33 @@
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.SocketException;
+import java.net.InetAddress;
 
 public class MainServer {
-    DatagramSocket ds;
 
-    {
-        try {
-            ds = new DatagramSocket(3989);
-        } catch (SocketException e) {
-            throw new RuntimeException(e);
-        }
+    public static void main(String[] args) throws Exception {
+
+        DatagramSocket ds = new DatagramSocket(3989);
+        System.out.println("il server attende");
+
+        byte[] buffer = new byte[256];
+
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+        ds.receive(packet);
+
+        String messaggio = new String(packet.getData(), 0, packet.getLength());
+        System.out.println("ricevuto: " + messaggio);
+
+        String risposta = "ho ricevuto: " + messaggio;
+        byte[] rispostaBytes = risposta.getBytes();
+
+        InetAddress clientAddress = packet.getAddress();
+        int clientPort = packet.getPort();
+
+        DatagramPacket rispostaPacket =
+                new DatagramPacket(rispostaBytes, rispostaBytes.length, clientAddress, clientPort);
+
+        ds.send(rispostaPacket);
+
+        ds.close();
     }
 }
