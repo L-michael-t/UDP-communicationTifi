8b0b01815b914e55a599539f4dc76ba1fdbe218d
diegoscuolaitis-tech <diegoscuolaitis@gmail.com>
2026-03-10 14:40:13 +0100

    commit

--- src/MainClient.java
+++ src/MainClient.java
@@ -1,17 +1,21 @@
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
+/*
+javadoc
+il client se viene avviato controlla se ce il server avviato/disponibile in caso negativo scrive server non disponibile
+in caso avviato dopo il server comincia la comunicazione UDP
+ */
+import java.net.*;
 
 public class MainClient {
 
     public static void main(String[] args) throws Exception {
 
         DatagramSocket ds = new DatagramSocket();
+        ds.setSoTimeout(5000); // attende massimo 5 secondi
 
-        String messaggio = "bella a tutti ragzzi";
+        String messaggio = "ciao server";
         byte[] buffer = messaggio.getBytes();
 
-        InetAddress serverAddress = InetAddress.getByName("tifuxhost");
+        InetAddress serverAddress = InetAddress.getByName("localhost");
 
         DatagramPacket packet =
                 new DatagramPacket(buffer, buffer.length, serverAddress, 3989);
@@ -22,10 +26,21 @@ public class MainClient {
         DatagramPacket rispostaPacket =
                 new DatagramPacket(rispostaBuffer, rispostaBuffer.length);
 
-        ds.receive(rispostaPacket);
+        try {
 
-        String risposta = new String(rispostaPacket.getData(), 0, rispostaPacket.getLength());
-        System.out.println("risposta dal server: " + risposta);
+            ds.receive(rispostaPacket);
+
+            String risposta = new String(
+                    rispostaPacket.getData(),
+                    0,
+                    rispostaPacket.getLength()
+            );
+
+            System.out.println("Risposta: " + risposta);
+
+        } catch (SocketTimeoutException e) {
+            System.out.println("Server non disponibile.");
+        }
 
         ds.close();
     }
